1f916e5d683ff54e06bbdf9f20e39ff00cf1fe6f
NGCyang <yangmeng06@gmail.com>
2016-11-29 20:59:50 -0500

    ranking mapper simple count

---
+++ OrgsRanking/OrgsRankingMapper.java
@@ -0,0 +1,34 @@
+/**
+ * Created by yangmeng on 11/8/16.
+ */
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.io.IntWritable;
+
+import org.apache.hadoop.mapreduce.Mapper;
+
+import org.json.*;
+
+
+public class JsonProfileMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException, JSONException{
+
+        JSONObject jsonobj = new JSONObject(value);
+        String type = jsonobj.getString("type");
+
+        if (jsonobj.has("org")
+                && (type.equals("fork")
+                    || type.equals("watch") )) {
+            JSONObject org = jsonobj.getJSONObject("org");
+            Long orgId = org.getLong("id");
+            String orgName = org.getString("login");
+            context.write(new Text(orgId + ":" + orgName), new IntWritable(1));
+        }
+    }
+
+}
